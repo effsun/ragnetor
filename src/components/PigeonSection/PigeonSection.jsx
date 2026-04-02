a87c34b71c576ec6b2627098922f4bdfebe7d291
effsun <effsun2@gmai.com>
2026-04-03 00:59:25 +0200

    review of style part

--- src/components/PigeonSection/PigeonSection.jsx
+++ src/components/PigeonSection/PigeonSection.jsx
@@ -4,10 +4,34 @@ import RobotModel from '../RobotModel/RobotModel'
 export default function PigeonSection() {
   return (
     <section className={styles.section}>
-      <p className={styles.text}>
-        PIGEONS WILL CAN NOT ESCAPE ANYMORE
-      </p>
+      <div className={styles.textBlock}>
+        <p className={styles.line}>
+          <span className={styles.filled}>PIGEONS</span>
+        </p>
+        <p className={styles.line}>
+          <span className={styles.dim}>WILL CAN NOT</span>
+        </p>
+        <p className={styles.line}>
+          <span className={styles.targetBox}>
+            <span className={styles.escape}>ESCAPE</span>
+            <span className={styles.bracketTL} />
+            <span className={styles.bracketTR} />
+            <span className={styles.bracketBL} />
+            <span className={styles.bracketBR} />
+          </span>
+        </p>
+        <p className={styles.line}>
+          <span className={styles.outline}>ANYMORE</span>
+        </p>
+      </div>
+
+      {/* Laser beam from text toward robot */}
+      <div className={styles.laser}>
+        <div className={styles.laserCore} />
+      </div>
+
       <div className={styles.robot}>
+        <div className={styles.robotRing} />
         <RobotModel />
       </div>
     </section>
